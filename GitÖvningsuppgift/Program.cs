26dc165b776b71d373253099c095135a76ebd094
Nicklas Eriksson <salk1n@hotmail.com>
2020-11-16 10:34:39 +0100

    Add project files.

---
+++ GitÖvningsuppgift/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitÖvningsuppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
