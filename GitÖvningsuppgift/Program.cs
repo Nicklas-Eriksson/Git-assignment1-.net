e109395d04d8ef1e65f63a12d0bda09a6319e56e
Nicklas Eriksson <salk1n@hotmail.com>
2020-11-24 12:34:47 +0100

    Added colour and beeps

--- GitÖvningsuppgift/Program.cs
+++ GitÖvningsuppgift/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace GitÖvningsuppgift
 {
@@ -9,14 +10,16 @@ namespace GitÖvningsuppgift
         static void Main(string[] args)
         {   //Titel
             Console.Title = "Movember Assignment";
-
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            
+            
             //Variables            
             double term1;
             double term2;
             double term3;
             double kalk1 = 0;
-            double sum = 0;            
-            List<double> sumList = new List<double>();            
+            double sum = 0;
+            List<double> sumList = new List<double>();
             char operator1;
             char operator2;
             string answer = "";
@@ -26,20 +29,25 @@ namespace GitÖvningsuppgift
             //Input from user
             Console.Write("Enter first operator: ");
             operator1 = Convert.ToChar(Console.ReadLine());
+            Console.Beep(400, 300);
             Console.WriteLine();
             Console.Write("Enter second operator: ");
             operator2 = Convert.ToChar(Console.ReadLine());
+            Console.Beep(400, 300);
             Console.WriteLine();
             Console.Write("Enter first term: ");
             term1 = Convert.ToDouble(Console.ReadLine());
+            Console.Beep(200, 300);
             Console.WriteLine();
             Console.Write("Enter second term: ");
             term2 = Convert.ToDouble(Console.ReadLine());
+            Console.Beep(200, 300);
             Console.WriteLine();
             Console.Write("Enter third term:  ");
             term3 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine();            
-            
+            Console.Beep(200, 300);
+            Console.WriteLine();
+
             while (isRunning == true)
             {   //For first part of the equation
                 bool firstRun = true;
@@ -47,25 +55,25 @@ namespace GitÖvningsuppgift
                 {
                     if (operator1 == '+')
                     {
-                        kalk1 = term1 + term2;                        
+                        kalk1 = term1 + term2;
                     }
                     else if (operator1 == '-')
                     {
-                        kalk1 = term1 - term2;                        
+                        kalk1 = term1 - term2;
                     }
                     else if (operator1 == '*')
                     {
-                        kalk1 = term1 * term2;                       
+                        kalk1 = term1 * term2;
                     }
                     else if (operator1 == '/')
                     {
-                        kalk1 = term1 / term2;                        
+                        kalk1 = term1 / term2;
                     }
 
                     //Operator round 2
                     if (operator2 == '+')
                     {
-                        sum = kalk1 + term3;                        
+                        sum = kalk1 + term3;
                     }
                     else if (operator2 == '-')
                     {
@@ -77,17 +85,17 @@ namespace GitÖvningsuppgift
                     }
                     else if (operator2 == '/')
                     {
-                        sum = kalk1 / term3;                        
+                        sum = kalk1 / term3;
                     }
 
                     sumList.Add(sum);// Adds the sum the the List
 
                     //Displays the calculation to the user
                     Console.Write($"{term1} {operator1} {term2} {operator2} {term3} = {sum}");
-                    
+
                     break;
                 }
-                
+
                 // Checks what the sum was
                 if (sum < 100)
                 {
@@ -96,14 +104,19 @@ namespace GitÖvningsuppgift
                     answer = Console.ReadLine();
                     if (answer == "y")
                     {
-                        Console.WriteLine("You'r answer is yes\n");                        
-                        goto start;                        
+                        Console.WriteLine("You'r answer is yes\n");
+                        goto start;
                     }
                     else if (answer == "n")
-                    {                        
+                    {
                         Console.WriteLine("Thank you for playing. The sum of all rounds is: " + sumList.Sum());
+                        Console.Beep(933, 300);
+                        Console.Beep(933, 100);
+                        Console.Beep(933, 100);
+                        Console.Beep(933, 100);
+                        Console.Beep(1047, 400);
                         break;
-                    }                    
+                    }
                 }
                 else if (sum == 100)
                 {
@@ -112,12 +125,17 @@ namespace GitÖvningsuppgift
                     answer = Console.ReadLine();
                     if (answer == "y")
                     {
-                        Console.WriteLine("You'r answer is yes\n");                        
+                        Console.WriteLine("You'r answer is yes\n");
                         goto start;
                     }
                     else if (answer == "n")
-                    {                        
+                    {
                         Console.WriteLine("Thank you for playing. The sum of all rounds is: " + sumList.Sum());
+                        Console.Beep(933, 300);
+                        Console.Beep(933, 100);
+                        Console.Beep(933, 100);
+                        Console.Beep(933, 100);
+                        Console.Beep(1047, 400);
                         break;
                     }
                 }
@@ -128,12 +146,17 @@ namespace GitÖvningsuppgift
                     answer = Console.ReadLine();
                     if (answer == "y")
                     {
-                        Console.WriteLine("You'r answer is yes\n");                        
+                        Console.WriteLine("You'r answer is yes\n");
                         goto start;
                     }
                     else if (answer == "n")
-                    {                        
+                    {
                         Console.WriteLine("Thank you for playing. The sum of all rounds is: " + sumList.Sum());
+                        Console.Beep(933, 300);
+                        Console.Beep(933, 100);
+                        Console.Beep(933, 100);
+                        Console.Beep(933, 100);
+                        Console.Beep(1047, 400);
                         break;
                     }
                 }
